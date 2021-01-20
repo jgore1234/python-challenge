66d7fb514516b806296968ab5293a39b6fbaccc6
John Gore <johngore@Johns-MBP.lan>
2021-01-19 22:56:31 -0800

    upload files

--- **PyBank**/main.py
+++ **PyBank**/main.py
@@ -1,129 +1,3 @@
-# #import pathlib module
-# import pathlib
-# import os
-# #create filepath for resource raw data
-# csvpath = pathlib.Path("Resources/budget_data.csv")
-
-# #import CSV reading module
-# import csv
-
-
-# with open(csvpath, mode='r') as csvfile:
-#     #set CSV reader delimeter and variable
-#     csvreader = csv.reader(csvfile, delimiter=',')
-
-#     #read header row first 
-#     csv_header = next(csvreader)
-#     print(f"CSV Header: {csv_header}")
-
-# # The total number of months included in the dataset
-#     # The total number of months is equal to the number of rows
-#     # excluding the header row
-#     # every time we call next, we increment a counter
-#     # count = 0
-#     # count = count + 1
-
-#     count = 0
-#     total_pnl = 0
-#     row = next(csvreader)
-#     # first_row = next(csvreader)
-#     change = []
-#     prior_pnl = 0
-#     months = []
-
-
-#     first_pnl = None
-#     last_pnl = None
-  
-#     # The stopping condition is that there is no data in the last row
-#     while row is not None:
-#         # Count the row
-#         count = count + 1
-#         months.append(row[0])
-#         # Get pnl for this month
-#         pnl = row[1]
-
-#         # Convert pnl from string to integer
-#         pnl = int(pnl)
-
-#         # Every row is one month
-#         # Count the pnl for this month
-#         total_pnl = total_pnl + pnl
-
-#         # Check if we have gotten the first pnl yet
-#         if first_pnl == None:
-#             first_pnl = pnl
-
-#         # Calculate change in PNL
-#         pnl_change = pnl - prior_pnl
-#         change.append(pnl_change)
-#         prior_pnl = pnl
-
-
-#         # Get the next row
-#         try:
-#             row = next(csvreader)
-#         except:
-#             break
-#     #    * The average of the changes in "Profit/Losses" over the entire period 
-#     avg_change = sum(change) / (count-1)
-
-#     #   * The greatest increase in profits (date and amount) over the entire period
-#     greatest_increase = max(change)
-#     greatest_increase_index = change.index(greatest_increase)
-#     greatest_increase_months = months[greatest_increase_index]
-    
-
-#     #   * The greatest decrease in losses (date and amount) over the entire period
-#     greatest_decrease = min(change)
-#     greatest_decrease_index = change.index(greatest_decrease)
-#     greatest_decrease_months = months[greatest_decrease_index]
-    
-#     print('Financial Analysis')
-#     print(count)
-#     print(total_pnl)
-#     # print(change)
-#     print(avg_change)
-#     print(greatest_decrease_months)
-#     print(greatest_decrease)
-#     print(greatest_increase_months)
-#     print(greatest_increase)
-
-          
-# Fin_Analysis_csv = pathlib.Path('/Analysis/Fin_Analysis.txt')
-
-# with open(Fin_Analysis_csv,'w') as outputfile:
- 
-
-#     Fin_Analysis = (
-#     f"\n\nFinancial Analysis\n"
-#     f"--------------------\n"
-#     f"Total Months: {count} \n"
-#     f"Total: ${total_pnl}\n"
-#     f"Average Change: ${avg_change}\n"
-#     f"Greatest Increase in Profits: {greatest_increase_months} (${greatest_increase})\n"
-#     f"Greatest Decrease in Profits: {greatest_decrease_months} (${greatest_decrease})\n"
-#     )   
-#     print(Fin_Analysis, end="")
-
-#     # outputfile.write(election_results)
-
-#     # for candidate in votes_summary.keys():
-#     #     voter_output = f"{candidate}: {votes_summary[candidate]['vote_pct']}% ({votes_summary[candidate]['votes']})\n"
-#     #     print(voter_output, end="")
-
-#     #     outputfile.write(voter_output)
-
-
-#     # winning_candidate_summary = (
-#     #     f"--------------------\n"
-#     #     f"Winner: {winner}\n"
-#     #     f"--------------------\n"
-#     # )
-#     # outputfile.write(wining_candidate_summary)
-
-#     # print(winning_candidate_summary)
-
 #import pathlib module
 import pathlib
 import os
@@ -145,7 +19,7 @@ with open(csvpath, mode='r') as csvfile:
 # The total number of months included in the dataset
     # The total number of months is equal to the number of rows
     # excluding the header row
-    # every time we call next, we increment a counter
+    # every time we call next, increment a counter
     # count = 0
     # count = count + 1
 
@@ -221,12 +95,12 @@ with open(csvpath, mode='r') as csvfile:
     print(greatest_increase_months)
     print(greatest_increase)
 
-          
+    #write the print statement to txt file      
 Fin_Analysis_csv = pathlib.Path('Analysis/Fin_Analysis.txt')
 
 with open(Fin_Analysis_csv,'w') as outputfile:
  
-
+    
     Fin_Analysis = (
     f"\n\nFinancial Analysis\n"
     f"--------------------\n"
@@ -240,30 +114,4 @@ with open(Fin_Analysis_csv,'w') as outputfile:
 
     print(Fin_Analysis, end="")
 
-     # ```text
-     #  Financial Analysis
-     #  ----------------------------
-     #  Total Months: 86
-     #  Total: $38382578
-     #  Average  Change: $-2315.12
-     #  Greatest Increase in Profits: Feb-2012 ($1926159)
-     #  Greatest Decrease in Profits: Sep-2013 ($-2196167)
-     #  ```
-
-    # outputfile.write(election_results)
-
-    # for candidate in votes_summary.keys():
-    #     voter_output = f"{candidate}: {votes_summary[candidate]['vote_pct']}% ({votes_summary[candidate]['votes']})\n"
-    #     print(voter_output, end="")
-
-    #     outputfile.write(voter_output)
-
-
-    # winning_candidate_summary = (
-    #     f"--------------------\n"
-    #     f"Winner: {winner}\n"
-    #     f"--------------------\n"
-    # )
-    # outputfile.write(wining_candidate_summary)
-
-    # print(winning_candidate_summary)
+    
--- **PyPoll**/main.py
+++ **PyPoll**/main.py
@@ -1,71 +1,53 @@
+#import pathlib module
 import pathlib
 import csv
 
-#import election data
-election_data_csv = pathlib.Path('Resources/election_data.csv')
+#create filepath for resource raw data
+csvpath = pathlib.Path('Resources/election_data.csv')
 
-with open(election_data_csv) as csvfile:
+with open(csvpath) as csvfile:
         csvreader = csv.reader(csvfile)
         csv_header = next(csvfile)
-
+        
         votes = {}
 
         for row in csvreader:
 
             #candidates vote count
-            candidate_name = row[2]
+            name = row[2]
      
-            if candidate_name in votes:
-                votes[candidate_name] += 1
+            if name in votes:
+                votes[name] += 1
             else:
-                votes[candidate_name] = 1
+                votes[name] = 1
 
 # print(votes)  
-vote_counts = (list(votes.values()))
+numberof_votes = (list(votes.values()))
 
 #total number of votes cast
-total_count = sum(vote_counts)
+total_count = sum(numberof_votes)
 # print(total_count)
 
 winner = list(votes.keys())[0]
-votes_summary = {}
+voting_summary = {}
 for candidate in votes.keys():
     if votes[candidate] > votes[winner]:
         winner = candidate
-    votes_summary[candidate] = {'votes': votes[candidate], 'vote_pct': round((votes[candidate]/total_count)*100,2),}
+    voting_summary[candidate] = {'votes': votes[candidate], 'vote_pct': round((votes[candidate]/total_count)*100,2),}
     if candidate == winner:
-        votes_summary[candidate]["is_winner"] = True
+        voting_summary[candidate]["is_winner"] = True
     else:
-        votes_summary[candidate]["is_winner"] = False
+        voting_summary[candidate]["is_winner"] = False
 
-# election_results = (
-#     f"\n\nElection Results\n"
-#     f"--------------------\n"
-#     f"Total Votes: {total_count}\n"
-#     f"--------------------\n"
-# )   
-# print(election_results, end="")
-
-# for candidate in votes_summary.keys():
-#     voter_output = f"{candidate}: {votes_summary[candidate]['vote_pct']}% ({votes_summary[candidate]['votes']})\n"
-#     print(voter_output, end="")
-
-
-# winning_candidate_summary = (
-#     f"--------------------\n"
-#     f"Winner: {winner}\n"
-#     f"--------------------\n"
-# )
-# print(winning_candidate_summary)
 
 
 election_results_csv = pathlib.Path('Analysis/election_results.txt')
-
+#write formated results to txt file in analysis folder
 with open(election_results_csv,'w') as outputfile:
     # csvwriter = csv.writer(outputfile)
 
     election_results = (
-    f"\n\nElection Results\n"
+    f"\n\nElection Results\n"   
     f"--------------------\n"
     f"Total Votes: {total_count}\n"
     f"--------------------\n"
@@ -74,65 +56,19 @@ with open(election_results_csv,'w') as outputfile:
 
     outputfile.write(election_results)
 
-    for candidate in votes_summary.keys():
-        voter_output = f"{candidate}: {votes_summary[candidate]['vote_pct']}% ({votes_summary[candidate]['votes']})\n"
+    for candidate in voting_summary.keys():
+        voter_output = f"{candidate}: {voting_summary[candidate]['vote_pct']}% ({voting_summary[candidate]['votes']})\n"
         print(voter_output, end="")
 
         outputfile.write(voter_output)
 
 
-    winning_candidate_summary = (
+    election_winner = (
         f"--------------------\n"
         f"Winner: {winner}\n"
         f"--------------------\n"
     )
-    outputfile.write(winning_candidate_summary)
+    outputfile.write(election_winner)
 
-    print(winning_candidate_summary)
-
-
-
-
-#  election_results_csv = pathlib.Path('Pypoll/Resources/election_results.csv')
-
-## {'Khan': {'votes': 2218231, 'vote_pct':...., 'is_winner': True}, 
-##'Correy': {'votes': 704200, 'vote_pct':...., 'is_winner': False}, 
-## 'Li': {'votes': 492940, 'vote_pct':...., 'is_winner': False},
-##  "O'Tooley": {'votes': 105630, 'vote_pct':...., 'is_winner': False}
-# }
-
-##   * The percentage of votes each candidate won
-
-# winner = 'list(votes.keys())[0]'
-
-
-
-
-
-# for candidate in votes_summary.keys():
-#     voter_output = f"{candidate}: {votes_summary[candidate]['vote_pct']}% ({votes_summary[candidate][votes]})\n"
-#     print(voter_output, end="")
-
-# winning_candidate_summary = (
-#     f"Winner: {winner}\n"
-# )
-#  print(winning_candidate_summary)
-
-#  election_results_csv = pathlib.Path('Pypoll/Resources/election_results.csv')
-
-#  with open(election_results_csv, 'w') as outputfile:
-#     #csvwriter = csv.writer(outputfile)
-#     election_results = (
-#         f"\n\nElection REsults\n"
-#     )
-
-#     print(election_results, end="")
-
-#     outputfile.write(election_results)
-
-#     for candidate in votes_summary.keys():
-#             voter_output = f"{candidate}: {votes_summary[candidate]['vote_pct']}% ({votes_summary[candidate]['votes']})\n"
-#             print(voter)
-
-# 4:41:21
+    print(election_winner)
 
